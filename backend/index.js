c1c94800bb23c4a0a14e128a5065e6b4f8798b5e
Chetan Rawat <153279462+ChetanRawat740@users.noreply.github.com>
2025-02-01 17:20:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,11 @@ app.get('/ping',(req,res)=>{
 });
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://deploy-mern-app-5h47.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+}));
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 
