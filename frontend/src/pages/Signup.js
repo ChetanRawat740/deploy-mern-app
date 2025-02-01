b36d0406671a4c7e0a7b56517ab6564a1ad0420f
Chetan Rawat <153279462+ChetanRawat740@users.noreply.github.com>
2025-02-01 16:56:43 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -30,7 +30,7 @@ function Signup() {
         return handleError('name,email,pasword are required')
     } 
     try {
-        const url =`https://deploy-mern-app-five.vercel.app//auth/signup`;
+        const url ="https://deploy-mern-app-five.vercel.app/auth/signup";
         // ';
         const response = await fetch(url, {
             method: "POST",
