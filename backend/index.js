a04a815b27c0ce625ae2c6006ba9c5b4c29bf76a
Chetan Rawat <153279462+ChetanRawat740@users.noreply.github.com>
2025-02-01 17:54:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,9 @@ app.get('/ping',(req,res)=>{
 });
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin:"*"
+    ));
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 
